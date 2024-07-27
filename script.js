c8fad3223efea6e63c166d18dab53930b2d60eda
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-07-27 20:21:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+console.log('Script start');
+
+// Macrotask using setTimeout
+setTimeout(() => {
+    console.log('Macrotask 1');
+}, 0);
+
+// Microtask using Promise
+Promise.resolve().then(() => {
+    console.log('Microtask 1');
+}).then(() => {
+    console.log('Microtask 2');
+});
+
+// Macrotask using setTimeout
+setTimeout(() => {
+    console.log('Macrotask 2');
+}, 0);
+
+// Microtask using Promise
+Promise.resolve().then(() => {
+    console.log('Microtask 3');
+});
+
+console.log('Script end');
